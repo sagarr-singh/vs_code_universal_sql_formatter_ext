5fedfd6a1dda9bbbc0d930b5323a36f411a038bb
sagarr-singh <sagarsatendrasingh408@gmail.com>
2026-03-19 22:18:24 +0530

    webpack compilation enabled

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  mode: "production",
+  entry: "./src/extension.ts",
+  output: {
+    path: path.resolve(__dirname, "out"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+  externals: {
+    vscode: "commonjs vscode",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
